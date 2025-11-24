c59e1881533b1b5c94f366ff503d5c226c3f3d34
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-11-24 11:15:29 +0800

    fix: setup

--- backend/routes/routes.js
+++
@@ -1,5 +0,0 @@
-import express from express
-import { userLogin } from "../controllers/controller.js"
-const router = express.router
-
-router.get('login',userLogin);
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import { userLogin, userSignup } from './controllers/controller.js';
-
+import cors from 'cors'
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 const app = express()
+await connectDB
 
+app.use(cors())
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000
-
 
 app.get('/',(req, res)=>{
     res.send('hello world')
 })
 
-app.get('/user/login', userLogin)
-app.get('/user/signup', userSignup)
+
 app.listen(PORT, () =>{
     console.log(`Server running at http://localhost:${PORT}`)
 })
